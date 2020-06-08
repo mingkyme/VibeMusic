64ac8e9ea102d97b3897cba2f42bfc87b1c05323
Mingky <mingkyme@gmail.com>
2020-06-09 00:40:53 +0900

    First

--- VibeMusic/ViewController.swift
+++ VibeMusic/ViewController.swift
@@ -7,18 +7,34 @@
 //
 
 import Cocoa
-
-class ViewController: NSViewController {
-
+import WebKit
+import MediaKeyTap
+class ViewController: NSViewController, MediaKeyTapDelegate {
+    var mediaKeyTap : MediaKeyTap?
+    @IBOutlet weak var web: WKWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        mediaKeyTap = MediaKeyTap(delegate: self)
+        mediaKeyTap?.start()
+        web.load(URLRequest.init(url: URL(string: "https://vibe.naver.com/")!))
         // Do any additional setup after loading the view.
     }
 
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
+        }
+    }
+    func handle(mediaKey: MediaKey, event: KeyEvent) {
+        switch mediaKey {
+            case .previous, .rewind:
+            web.evaluateJavaScript("document.querySelector('#player > div.player_controller > div.control_section > div > a.btn_play_prev').click()", completionHandler: nil)
+            
+        case .playPause:
+            web.evaluateJavaScript("document.querySelector('#player > div.player_controller > div.control_section > div > a.btn_now').click()", completionHandler: nil)
+        
+        case .next, .fastForward:
+            web.evaluateJavaScript("document.querySelector('#player > div.player_controller > div.control_section > div > a.btn_play_next').click()", completionHandler: nil)
         }
     }
 
