e8e9eba38b1448718574e1d2829dc029f9f9e407
Mingky <mingkyme@gmail.com>
2020-06-09 00:24:52 +0900

    Initial Commit

---
+++ VibeMusic/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  VibeMusic
+//
+//  Created by 김민기 on 2020/06/09.
+//  Copyright © 2020 Mingky. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ VibeMusic/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VibeMusic
+//
+//  Created by 김민기 on 2020/06/09.
+//  Copyright © 2020 Mingky. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
